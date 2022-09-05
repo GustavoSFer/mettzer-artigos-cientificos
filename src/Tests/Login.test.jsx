272d9daea202ed51a86883e8276c691749892038
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-09-05 16:10:33 -0300

    Test: Verificando o comportamento da tela de login

--- src/Tests/Login.test.jsx
+++ src/Tests/Login.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 /* globals describe, expect, it */
@@ -19,5 +20,33 @@ describe('Verificando tela de login, se os elementos estão na tela', () => {
     render(<MemoryRouter><App /></MemoryRouter>);
     const buttons = screen.getAllByRole('button');
     expect(buttons).toHaveLength(2);
+  });
+});
+
+describe('Verificando comportamentos na tela de "login"', () => {
+  it(`Verificando se quando digitado o email e senha 
+  realmente aparece o que foi escrito`, () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const inputEmail = screen.getByTestId('email');
+    const inputPassword = screen.getByTestId('password');
+
+    userEvent.type(inputEmail, 'gustavo@gmail.com');
+    userEvent.type(inputPassword, '3432fdsgsd');
+
+    expect(inputEmail).toHaveValue('gustavo@gmail.com');
+    expect(inputPassword).toHaveValue('3432fdsgsd');
+  });
+  it('Verificando se quando clica em "Entrar" é redirecionado para /main', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const inputEmail = screen.getByTestId('email');
+    const inputPassword = screen.getByTestId('password');
+    const btnEntrar = screen.getByTestId('btn-entrar');
+
+    userEvent.type(inputEmail, 'gustavo@gmail.com');
+    userEvent.type(inputPassword, '3432fdsgsd');
+    userEvent.click(btnEntrar);
+
+    const inputSearch = screen.getByTestId('search');
+    expect(inputSearch).toBeInTheDocument();
   });
 });
